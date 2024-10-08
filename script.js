a96ac097aa12ed9ad00803997d4b69f87330d66d
chris samuel <cosalcedo@up.edu.ph>
2024-10-08 14:16:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,31 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-$(document).ready(function() {
-  if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-    $('.card').on('click', function() {
-      $(this).addClass('zoomed');
-      $('#overlay').show();
+$(document).ready(function () {
+  if (
+    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    $(".card").on("click", function () {
+      $(this).addClass("zoomed");
+      $("#overlay").show();
     });
 
-    $('.card canvas').on('click', function(event) {
+    $(".card canvas").on("click", function (event) {
       event.stopPropagation();
     });
 
-    $('#overlay').on('click', function() {
-      $('.zoomed').removeClass('zoomed');
+    $("#overlay").on("click", function () {
+      $(".zoomed").removeClass("zoomed");
       $(this).hide();
     });
 
-    $(document).on('keyup', function(event) {
-      if (event.keyCode === 27) { // 27 is the key code for the escape key
-        $('.zoomed').removeClass('zoomed');
-        $('#overlay').hide();
+    $(document).on("keyup", function (event) {
+      if (event.keyCode === 27) {
+        // 27 is the key code for the escape key
+        $(".zoomed").removeClass("zoomed");
+        $("#overlay").hide();
       }
     });
   }
