17fec237711a89692503736c25f44d3ee3fed610
Ann Junah Suyman <118262757+ghjlmnsy@users.noreply.github.com>
2024-11-09 00:14:53 +0800

    refactored code

--- faculty_script.js
+++ faculty_script.js
@@ -6,157 +6,159 @@ function fetchChartData(localhost, callback) {
   fetch(localhost)
     .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response error.');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .then((data) => callback(data))
-    // .catch((error) => console.error("Error fetching data:", error));
+    .then((data) => callback(data));
+  // .catch((error) => console.error("Error fetching data:", error));
 }
 
-
+// Main function to render all charts using the fetched data
 function renderCharts(data) {
   renderRatioByRank(data.ratioByRank);
   renderRatioByEduc(data.ratioByEduc);
   renderNumberOfTotalFaculty(data.numberOfTotalFaculty);
   renderNumberOfPublications(data.numberOfPublications);
-  renderFacultySembyRank(data.facultySembyRank)
-  renderFacultyByEducAttainment(data.facultyByEducAttainment)
+  renderFacultySembyRank(data.facultySembyRank);
+  renderFacultyByEducAttainment(data.facultyByEducAttainment);
 }
 
-let ratioByRankChart;
+// Chart variables for handling re-rendering
+var ratioByRankChart, ratioByEducChart, totalFacultyChart, numberOfPublicationsChart, facultySembyRankChart, facultyByEducAttainmentChart;
 
 function renderRatioByRank(chartData) {
   var ctx = document.getElementById("ratioByRank").getContext("2d");
-
   var datasets = chartData.map((item, index) => {
     return {
       label: item.facultyRank,
       data: [item.rankCount],
-      backgroundColor: ["#8E1537", "#FFB81D", "#005740", "#8E1537", "#C70039"][index % 5],
+      backgroundColor: ["#8E1537", "#FFB81D", "#005740", "#8E1537", "#C70039"][
+        index % 5
+      ],
     };
   });
-
   ratioByRankChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: ['Ratio by Rank'],
+      labels: ["Ratio by Rank"],
       datasets: datasets,
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
+          position: "bottom",
+        },
+      },
     },
   });
 }
 
-let ratioByEducChart;
-
 function renderRatioByEduc(chartData) {
   var ctx = document.getElementById("ratioByEduc").getContext("2d");
-
   var datasets = chartData.map((item, index) => {
     return {
       label: item.educationalAttainment,
       data: [item.facultyCount],
-      backgroundColor: ["#8E1537", "#FFB81D", "#005740", "#8E1537", "#C70039"][index % 5],
+      backgroundColor: ["#8E1537", "#FFB81D", "#005740", "#8E1537", "#C70039"][
+        index % 5
+      ],
     };
   });
-
   ratioByEducChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: ['Ratio by Education'],
+      labels: ["Ratio by Education"],
       datasets: datasets,
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
+          position: "bottom",
+        },
+      },
     },
   });
 }
-
-let totalFacultyChart;
 
 function renderNumberOfTotalFaculty(chartData) {
-  var labels = chartData.map((item) => item.SchoolYear + " Sesmester " + item.semester);
+  var labels = chartData.map(
+    (item) => item.SchoolYear + " Sesmester " + item.semester
+  );
   var totalFaculty = chartData.map((item) => item.totalFaculty);
-  var customColors = ['#8E1537', '#FFB81D', '#005740', '#808080'];
-
-  var ctx = document.getElementById('numberOfTotalFaculty').getContext("2d");
+  var customColors = ["#8E1537", "#FFB81D", "#005740", "#808080"];
+  var ctx = document.getElementById("numberOfTotalFaculty").getContext("2d");
   totalFacultyChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: labels,
-      datasets: [{
-        label: 'Total Faculty',
-        data: totalFaculty,
-        backgroundColor: customColors,
-        borderColor: customColors,
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: "Total Faculty",
+          data: totalFaculty,
+          backgroundColor: customColors,
+          borderColor: customColors,
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
-    }
+          position: "bottom",
+        },
+      },
+    },
   });
 }
-
-let numberOfPublicationsChart;
 
 function renderNumberOfPublications(chartData) {
-  var labels = chartData.map((item) => item.SchoolYear + ' Semester ' + item.semester);
+  var labels = chartData.map(
+    (item) => item.SchoolYear + " Semester " + item.semester
+  );
   var counts = chartData.map((item) => item.totalPublications);
-  var customColors = ['#8E1537', '#FFB81D', '#005740', '#808080'];
-
-  var ctx = document.getElementById('numberOfPublications').getContext("2d");
+  var customColors = ["#8E1537", "#FFB81D", "#005740", "#808080"];
+  var ctx = document.getElementById("numberOfPublications").getContext("2d");
   numberOfPublicationsChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: labels,
-      datasets: [{
-        label: 'Publications',
-        data: counts,
-        backgroundColor: customColors,
-        borderColor: customColors,
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: "Publications",
+          data: counts,
+          backgroundColor: customColors,
+          borderColor: customColors,
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
-    }
+          position: "bottom",
+        },
+      },
+    },
   });
 }
 
-let facultySembyRankChart;
-
 function renderFacultySembyRank(chartData) {
-  // Create a set to hold unique semester labels
   var labelSet = new Set();
   var datasets = {};
+  var customColors = [
+    "#8E1537",
+    "#FFB81D",
+    "#005740",
+    "#C70039",
+    "#0066cc",
+    "#ff8c00",
+  ];
 
-  // Define custom colors for each rank
-  var customColors = ['#8E1537', '#FFB81D', '#005740', '#C70039', '#0066cc', '#ff8c00'];
-
-  // Loop through the chart data to populate the label set and initialize datasets
   chartData.forEach((item, index) => {
     var semesterLabel = item.SchoolYear + " " + item.semester;
     labelSet.add(semesterLabel);
@@ -166,15 +168,13 @@ function renderFacultySembyRank(chartData) {
         data: [],
         backgroundColor: customColors[index % customColors.length], // Use modulo to cycle through colors
         borderColor: customColors[index % customColors.length],
-        borderWidth: 1
+        borderWidth: 1,
       };
     }
   });
 
   // Convert the set of labels to an array
   var labels = Array.from(labelSet);
-
-  // Initialize data arrays for each rank
   for (var rank in datasets) {
     for (var i = 0; i < labels.length; i++) {
       datasets[rank].data.push(0); // Initialize all data points to 0
@@ -188,15 +188,14 @@ function renderFacultySembyRank(chartData) {
     datasets[item.Rank].data[labelIndex] = item.facultyCount;
   });
 
-  // Convert datasets object to array
   var datasetsArray = Object.values(datasets);
 
-  var ctx = document.getElementById('facultySembyRank').getContext("2d");
+  var ctx = document.getElementById("facultySembyRank").getContext("2d");
   facultySembyRankChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: labels,
-      datasets: datasetsArray
+      datasets: datasetsArray,
     },
     options: {
       responsive: true,
@@ -204,37 +203,32 @@ function renderFacultySembyRank(chartData) {
         x: {
           title: {
             display: true,
-            text: 'Semesters'
-          }
+            text: "Semesters",
+          },
         },
         y: {
           title: {
             display: true,
-            text: ''
+            text: "",
           },
-        }
+        },
       },
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
-    }
+          position: "bottom",
+        },
+      },
+    },
   });
 }
 
-let facultyByEducAttainmentChart;
-
 function renderFacultyByEducAttainment(chartData) {
-  // Extract unique labels for semesters
-  var labels = [...new Set(chartData.map((item) => item.SchoolYear + " " + item.semester))];
-  
+  var labels = [
+    ...new Set(chartData.map((item) => item.SchoolYear + " " + item.semester)),
+  ];
   var datasets = {};
-  
-  // Define custom colors for each educational attainment
-  var customColors = ['#8E1537', '#FFB81D', '#005740', '#C70039'];
+  var customColors = ["#8E1537", "#FFB81D", "#005740", "#C70039"];
 
-  // Loop through the chart data to create datasets for each educational attainment
   chartData.forEach((item) => {
     if (!datasets[item.EducationalAttainment]) {
       datasets[item.EducationalAttainment] = {
@@ -243,7 +237,7 @@ function renderFacultyByEducAttainment(chartData) {
         backgroundColor: customColors,
         borderColor: customColors,
         borderWidth: 1,
-        fill: false
+        fill: false,
       };
     }
     var labelIndex = labels.indexOf(item.SchoolYear + " " + item.semester);
@@ -252,69 +246,61 @@ function renderFacultyByEducAttainment(chartData) {
     }
   });
 
-  // Convert datasets object to array
   var datasetsArray = Object.values(datasets);
 
-  var ctx = document.getElementById('facultyByEducAttainment').getContext("2d");
+  var ctx = document.getElementById("facultyByEducAttainment").getContext("2d");
   facultyByEducAttainmentChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: labels,
-      datasets: datasetsArray
+      datasets: datasetsArray,
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'bottom'
-        }
-      }
-    }
+          position: "bottom",
+        },
+      },
+    },
   });
 }
 
-$(document).ready(function() {
-  $('#filterButton').on('submit', function(e) {
-      e.preventDefault();
+// Event listener for filter button, used to fetch data based on filter criteria
+$(document).ready(function () {
+  $("#filterButton").on("submit", function (e) {
+    e.preventDefault();
+    var fromYear = $("#fromYear").val();
+    var toYear = $("#toYear").val();
+    var fromSemester = $("#fromSemester").val();
+    var toSemester = $("#toSemester").val();
 
-      var fromYear = $('#fromYear').val();
-      var toYear = $('#toYear').val();
-      var fromSemester = $('#fromSemester').val();
-      var toSemester = $('#toSemester').val();
+    console.log("Inputted in Form", fromYear, toYear, fromSemester, toSemester);
 
-      console.log("Inputted in Form", fromYear, toYear, fromSemester, toSemester);
+    $.ajax({
+      url: "getchart_faculty.php",
+      method: "POST",
+      data: {
+        fromYear: fromYear,
+        toYear: toYear,
+        fromSemester: fromSemester,
+        toSemester: toSemester,
+      },
+      success: function (response) {
+        var data = JSON.parse(response);
+        rerenderCharts(data);
+      },
+    });
+  });
+});
 
-      $.ajax({
-          url: 'getchart_faculty.php',
-          method: 'POST',
-          data: {
-              fromYear: fromYear,
-              toYear: toYear,
-              fromSemester: fromSemester,
-              toSemester: toSemester
-          },
-          success: function(response) {
-              var data = JSON.parse(response);
-              rerenderCharts(data);
-          }
-      });})});
-
+// Re-render all charts after data filter is applied
 function rerenderCharts(data) {
-  totalFacultyChart.destroy();
-  renderNumberOfTotalFaculty(data.numberOfTotalFaculty);
-
-  numberOfPublicationsChart.destroy();
-  renderNumberOfPublications(data.numberOfPublications);
-
-  facultySembyRankChart.destroy();
-  renderFacultySembyRank(data.facultySembyRank);
-
-  facultyByEducAttainmentChart.destroy();
-  renderFacultyByEducAttainment(data.facultyByEducAttainment);
-
-  ratioByRankChart.destroy();
-  renderRatioByRank(data.ratioByRank);
-
-  ratioByEducChart.destroy();
-  renderRatioByEduc(data.ratioByEduc);
+  if (totalFacultyChart) totalFacultyChart.destroy();
+  if (numberOfPublicationsChart) numberOfPublicationsChart.destroy();
+  if (facultySembyRankChart) facultySembyRankChart.destroy();
+  if (facultyByEducAttainmentChart) facultyByEducAttainmentChart.destroy();
+  if (ratioByRankChart) ratioByRankChart.destroy();
+  if (ratioByEducChart) ratioByEducChart.destroy();
+  renderCharts(data);
 }
