a912172ba5a9e9d6396eae6f22efc43c2eb02fbf
Ann Junah Suyman <118262757+ghjlmnsy@users.noreply.github.com>
2024-11-08 23:40:31 +0800

    refactored code

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
+// Function to open the navigation sidebar by setting its width to 350px
 function openNav() {
   document.getElementById("mySidenav").style.width = "350px";
 }
 
+// Function to close the navigation sidebar by setting its width to 0
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-$(document).ready(function () {
-  if (
-    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    $(".card").on("click", function () {
-      $(this).addClass("zoomed");
-      $("#overlay").show();
+// Runs when the document is fully loaded
+$(document).ready(function() {
+  // Check if the device is not a mobile device by testing the user agent
+  if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+    
+    // When a card element is clicked, add the 'zoomed' class and display the overlay
+    $('.card').on('click', function() {
+      $(this).addClass('zoomed');
+      $('#overlay').show();
     });
 
-    $(".card canvas").on("click", function (event) {
+    // Prevent canvas clicks inside the card from propagating to the card click event
+    $('.card canvas').on('click', function(event) {
       event.stopPropagation();
     });
 
-    $("#overlay").on("click", function () {
-      $(".zoomed").removeClass("zoomed");
+    // When the overlay is clicked, remove the 'zoomed' class from the card and hide the overlay
+    $('#overlay').on('click', function() {
+      $('.zoomed').removeClass('zoomed');
       $(this).hide();
     });
 
-    $(document).on("keyup", function (event) {
-      if (event.keyCode === 27) {
-        // 27 is the key code for the escape key
-        $(".zoomed").removeClass("zoomed");
-        $("#overlay").hide();
+    // Listen for the 'Escape' key to close any zoomed card and hide the overlay
+    $(document).on('keyup', function(event) {
+      if (event.keyCode === 27) { // 27 is the key code for the escape key
+        $('.zoomed').removeClass('zoomed');
+        $('#overlay').hide();
       }
     });
   }
